32df9948c007bdb7be7e33d9ead6717a3a351317
jrockb87 <jrockb@gmail.com>
2021-07-15 17:31:16 -0500

    agregado componente p-card, ejemplo pipes uppercase, lowercase y titlecase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { AppRouterModule } from './app-router.module';
+import { VentasModule } from './ventas/ventas.module';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { AppRouterModule } from './app-router.module';
   imports: [
     BrowserModule,
     AppRouterModule,
-    SharedModule // shared module importa prime-ng.module ya que ahí lo usa
+    SharedModule, // shared module importa prime-ng.module ya que ahí lo usa
+    VentasModule
   ],
   providers: [],
   bootstrap: [AppComponent]
