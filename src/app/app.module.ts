2ba15de1c456754257aa773310bc58340537a582
jrockb87 <jrockb@gmail.com>
2021-07-15 19:33:38 -0500

    cambio de formato de fecha a español de Colombia

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -6,6 +6,10 @@ import { SharedModule } from './shared/shared.module';
 import { AppRouterModule } from './app-router.module';
 import { VentasModule } from './ventas/ventas.module';
 
+// cambiar el locale de la app de manera global
+import localEsCo from '@angular/common/locales/es-CO'; // el nombre del import es arbitrario
+import { registerLocaleData } from '@angular/common' // importar la función
+registerLocaleData(localEsCo);
 
 @NgModule({
   declarations: [
@@ -17,7 +21,9 @@ import { VentasModule } from './ventas/ventas.module';
     SharedModule, // shared module importa prime-ng.module ya que ahí lo usa
     VentasModule
   ],
-  providers: [],
+  providers: [ // registrar el locale de manera global
+    { provide: LOCALE_ID, useValue: 'es-CO' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
