2163343b514f5e6b3584f0dba6886a6302d45d20
jrockb87 <jrockb@gmail.com>
2021-07-17 14:14:06 -0500

    Agregado ejemplo del pipe i18nPlural

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -8,12 +8,21 @@ import { Component } from '@angular/core';
 })
 export class NoComunesComponent {
 
+  // i18nSelect
   nombre: string = 'Diana';
   genero: string = 'femenino';
-
   invitacionMap = { // objeto para las opciones de transformacion del i18Select pipe
     'masculino': 'invitarlo',
     'femenino': 'invitarla'
+  };
+
+  // i18nPlural
+  clientes: string[] = ['Maria', 'Pedro', 'Luis'];
+  clientesMap = {
+    '=0': 'No tenemos ningun cliente esperando',
+    '=1': 'tenemos un cliente esperando',
+    '=2': 'tenemos 2 clientes esperando',
+    'other': 'tenemos # clientes esperando'
   };
 
 }
