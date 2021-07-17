4c2155077b6f899249061922c97cde27c3548649
jrockb87 <jrockb@gmail.com>
2021-07-17 18:15:04 -0500

    Agregado ejemplo de Async pipe

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-no-comunes',
@@ -42,10 +43,39 @@ export class NoComunesComponent {
   }
 
   // keyValue pipe
+  // tslint:disable-next-line: member-ordering
   persona = {
     nombre: 'Jonny',
     edad: 33,
     direccion: 'Bogota, Colombia'
   };
+
+  // Json pipe
+  // tslint:disable-next-line: member-ordering
+  heroes = [
+    {
+      nombre : 'Superman',
+      vuela : true
+    },
+    {
+      nombre : 'Batman',
+      vuela : false
+    },
+    {
+      nombre : 'Aquaman',
+      vuela : false
+    }
+  ];
+
+  // Async Pipe
+  // tslint:disable-next-line: member-ordering
+  miObservable = interval(1000); // observable que va a emitir numeros cada segundo
+
+  // tslint:disable-next-line: member-ordering
+  valorPromesa = new Promise( (resolve, reject) => {
+    setTimeout(()  => { // llama la función resolve cada 3500 ms
+      resolve('Tenemos data de promesa');
+    }, 3500);
+  });
 
 }
