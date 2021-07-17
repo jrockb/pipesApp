f8638e132255a4fc2de3a6752edd3ae5acd68bfa
jrockb87 <jrockb@gmail.com>
2021-07-17 13:38:19 -0500

    agregado ejempĺo del pipe i18nSelect

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-no-comunes',
@@ -6,11 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class NoComunesComponent implements OnInit {
+export class NoComunesComponent {
 
-  constructor() { }
+  nombre: string = 'Diana';
+  genero: string = 'femenino';
 
-  ngOnInit(): void {
-  }
+  invitacionMap = { // objeto para las opciones de transformacion del i18Select pipe
+    'masculino': 'invitarlo',
+    'femenino': 'invitarla'
+  };
 
 }
